2d87f15d28f17d246c51370ab351d573f4d4917e
Daniel Aranda <javierdaniel@gmail.com>
2015-02-22 11:54:22 -0500

    Adding phpunit.xml to the git repo

---
+++ tests/RESTful/Base.php
@@ -0,0 +1,15 @@
+<?php
+namespace RESTful\Test;
+
+/**
+ * RESTful - Standalone RESTful server library
+ * @author: Daniel Aranda (https://github.com/daniel-aranda/)
+ *
+ */
+
+abstract class Base extends \PHPUnit_Framework_TestCase
+{
+
+
+
+}
