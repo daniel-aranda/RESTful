9e6bff929ed6f05785b38a32121652fafd90dc90
Daniel Aranda <javierdaniel@gmail.com>
2015-02-22 00:45:05 -0500

    Setting up PHP Unit

---
+++ core/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+define('RESTful_PATH', __DIR__ . DIRECTORY_SEPARATOR);
+
+require RESTful_PATH . '/../vendor/autoload.php';
---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+define('RESTful_PATH_TEST', __DIR__ . '/');
+
+require RESTful_PATH_TEST . '/../core/bootstrap.php';
