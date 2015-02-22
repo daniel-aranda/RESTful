a0fefc066f5269d8e3d57500fc82951528b00351
Daniel Aranda <javierdaniel@gmail.com>
2015-02-21 21:45:10 -0500

    api.php created in public folder

---
+++ public/api.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * RESTful - Standalone RESTful server library
+ * @author: Daniel Aranda (https://github.com/daniel-aranda/)
+ *
+ * This file(api.php) should be set in the public folder of the server and include the autoload from bootstrap, that
+ * is all that you need to have it running. Just double checking something obvious, please remember that composer folder
+ * should NOT be in the public folder.
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
